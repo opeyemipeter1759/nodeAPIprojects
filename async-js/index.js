8ba70262c6c6639f764fa60d6f43232507e5d99f
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2022-01-24 15:01:28 +0100

    add catch to handle the err

--- async-js/index.js
+++ async-js/index.js
@@ -7,15 +7,14 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   // get dog api
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
+    .then((res) => {
       if (err) return console.log(err.message)
 
       console.log(`dog image : ${res.body.message}`)
 
-        fs.writeFile( "dog-img.txt", res.body.message, ( err ) =>
-        {
-            if ( err ) return console.log( err.message )
-            console.log( "Random dog image  saved" )
-        })
-    })
+      fs.writeFile("dog-img.txt", res.body.message, (err) => {
+        if (err) return console.log(err.message)
+        console.log("Random dog image  saved")
+      })
+    }).catch((err)=>{ if (err) return console.log(err.message)})
 })
