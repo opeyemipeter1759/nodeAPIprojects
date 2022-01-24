171b59f9d432b6289f5139b5e761b3f2fe4ffc7f
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2022-01-24 14:26:00 +0100

    create file in async

--- how-node-works/event-loop.js
+++ how-node-works/event-loop.js
@@ -1,18 +1,14 @@
-const fs = require( 'fs' )
-setTimeout( () => console.log( 'timer 1 finished' ), 0 )
-setImmediate( () => console.log( 'immediate 1 finished' ) )
+const fs = require("fs")
+setTimeout(() => console.log("timer 1 finished"), 0)
+setImmediate(() => console.log("immediate 1 finished"))
 
-fs.readFile( "test-file.txt", () =>
-{
-    console.log( "I/O file read finished" )
-    console.log( "----------------------------------------------------------------"
-    )
+fs.readFile("test-file.txt", () => {
+  console.log("I/O file read finished")
+  console.log(
+    "----------------------------------------------------------------"
+  )
 
-    setTimeout( () => console.log( "timer 2 finished" ), 0 )
-    setTimeout(() => console.log("timer 3 finished"), 3000)
-setImmediate(() => console.log("immediate 2 finished"))
-
-    
-} )
-
-
+  setTimeout(() => console.log("timer 2 finished"), 0)
+  setTimeout(() => console.log("timer 3 finished"), 3000)
+  setImmediate(() => console.log("immediate 2 finished"))
+})
