293ac2de37581d611fc85ca239c6c762d88bee52
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2022-07-02 11:14:01 +0100

    resolving promises

---
+++ JS/U-callstack/promises.js
@@ -0,0 +1,39 @@
+const fakeRequest = ( url ) => {
+    return new Promise( ( resolve, reject ) => {
+        setTimeout( () =>
+        {
+            const pages = {
+                "/users": [
+                    { id: 1, name: "John" },
+                    { id: 2, name: "Sara" },
+                    { id: 3, name: "Bill" }
+                ],
+                "/about": "About page",
+            }
+            const data = pages[url]
+            resolve( {
+                    status: 200, data
+                } )
+                reject( {
+                    status: 404
+                } )
+        }, 1000 )
+    } )
+}
+
+fakeRequest("/users")
+    .then( ( res ) =>
+    {
+        console.log("status code :", res.status )
+        console.log( "status code :", res.data )
+
+    } )
+    .catch( ( res ) => console.log( "status code :", res.status ) )
+
+    fakeRequest( "/about" )
+        .then( ( res ) => {
+            console.log( "status code :", res.status )
+            console.log( "status code :", res.data )
+
+        } )
+        .catch( ( res ) => console.log( "status code :", res.status ) )
